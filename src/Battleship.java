34d619afc6ff67f5ba2f42bf819b516bcaa8724a
Konstantine Gadyrka <kos.gdk@gmail.com>
2016-02-08 23:49:43 +0300

    Working on...

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,10 +1,12 @@
 import Exceptions.GameOverException;
 
+import java.util.Scanner;
+
 public class Battleship {
 
     public static void main(String[] args) {
 
-        Field field = new Field(10, 10);
+        Field field = new Field(10, 10, true);
         GameController gameController = new GameController(field);
         Computer computer = new Computer(field);
 
@@ -27,15 +29,19 @@ public class Battleship {
 
         field.drawField();
 
-        try{
-            for (int i = 0; i < 100; i++) {
-                computer.makeShot();
-                gameController.checkGameOver();
-            }
-        }catch (GameOverException e){
-            System.out.println("Game over");
+        Scanner scanner = new Scanner(System.in);
 
+        String test = "";
+
+        for (int i = 0; i < 100; i++) {
+            computer.makeShot();
+//            test = scanner.nextLine();
+            if (gameController.checkGameOver()){
+                System.out.println("Game over");
+                break;
+            }
         }
+
 
     }
 }
